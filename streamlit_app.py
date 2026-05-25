7d2a93d8c5dc6b37811a84e9c9eb92f86f6c9d65
Saloni Giratkar <giratkarsaloni1@gmail.com>
2026-05-25 16:29:06 +0530

    Refactor requests import and API call format

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests 
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -44,8 +44,10 @@ if ingredients_list:
     # if ingredients_string:
     #     session.sql(my_insert_stmt).collect()
     #     st.success('Your Smoothie is ordered!', icon="✅")
-    import requests  
-    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+ 
+    smoothiefroot_response = requests.get(
+        "https://my.smoothiefroot.com/api/fruit/watermelon"
+    )  
     st.text(smoothiefroot_response.json())
     # sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
   
